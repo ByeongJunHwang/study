bd26b8d6787a291959cde1a0d77bd77295ead04a
ByeongJunHwang <qudwnsghkd10@gmail.com>
2017-06-26 16:59:24 +0900

    hello.java modified

--- hello.java
+++ hello.java
@@ -1,9 +1,13 @@
+//For command line git tutorial
+//Github modification
+
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		System.out.println("Hello world");
 		System.out.println("Tell your world");
 		System.out.println("hi");
+		
 	}
 }
 
